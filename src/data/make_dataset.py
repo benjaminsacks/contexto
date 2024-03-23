38243307d9a795f7a12819acc568d90a3edd2a05
Benjamin Sacks <nadoben21@gmail.com>
2024-03-22 17:44:25 -0700

    Added pkl to save loaded data Added TODOs

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -12,6 +12,7 @@ from tqdm import tqdm
 
 GLOVE_LENGTH = 400_000
 
+
 def parse_glove_data(filepath, progress_bar=False):
     """
     Parse GloVe embedding data from a text file.
@@ -31,87 +32,65 @@ def parse_glove_data(filepath, progress_bar=False):
                 coefs = np.fromstring(coefs, "f", sep=" ")
                 embeddings_index[word] = coefs
         else:
-            for line in tqdm(f, desc='Reading file', unit='line', total=GLOVE_LENGTH):
+            for line in tqdm(f, desc="Reading file", unit="line", total=GLOVE_LENGTH):
                 word, coefs = line.split(maxsplit=1)
                 coefs = np.fromstring(coefs, "f", sep=" ")
                 embeddings_index[word] = coefs
 
     return embeddings_index
 
+
 def get_words_array(embeddings_index):
-    """
-    Convert keys of embeddings_index dictionary to a numpy array.
-
-    Parameters:
-    - embeddings_index (dict): A dictionary containing word embeddings.
-
-    Returns:
-    - numpy.array: Array of words from the embeddings_index keys.
-    """
     return np.array(list(embeddings_index.keys()))
 
+
 def filter_alphabetic(words_array):
-    """
-    Filter out non-alphabetic words from an array.
-
-    Parameters:
-    - words_array (numpy.array): Array of words to filter.
-
-    Returns:
-    - numpy.array: Filtered array containing only alphabetic words.
-    """
-    pattern = re.compile(r'^[a-z]+$')
+    pattern = re.compile(r"^[a-z]+$")
     return np.array([x for x in words_array if pattern.match(x)])
 
+
 def filter_20k(words_array, filepath_20k):
-    """
-    Filter words array based on the top 20,000 most common words.
-
-    Parameters:
-    - words_array (numpy.array): Array of words to filter.
-    - filepath_20k (str): Path to a text file containing the top 20,000 most common English words.
-
-    Returns:
-    - numpy.array: Array containing words from words_array that are in the top 20,000 list.
-    """
     with open(filepath_20k, "r") as file:
         top_20k = file.read().splitlines()
     return np.intersect1d(words_array, top_20k)
 
+
 def embeddings_to_dataframe(embeddings_index, words_array=None):
-    """
-    Convert word embeddings dictionary to a pandas DataFrame.
-
-    Parameters:
-    - embeddings_index (dict): Dictionary containing word embeddings.
-    - words_array (numpy.array or None): Array of words to filter embeddings. If None, all embeddings are used.
-
-    Returns:
-    - pandas.DataFrame: DataFrame containing word embeddings with words as index.
-    """
     if words_array is None:
         return pd.DataFrame.from_dict(embeddings_index, orient="index")
     else:
-        filtered_embeddings = {word: embeddings_index[word] for word in words_array if word in embeddings_index}
+        filtered_embeddings = {
+            word: embeddings_index[word]
+            for word in words_array
+            if word in embeddings_index
+        }
         return pd.DataFrame.from_dict(filtered_embeddings, orient="index")
 
+# TODO : Fix commented section. Either remove or implement
+# @click.command()
+# @click.argument("input_filepath", type=click.Path(exists=True))
+# @click.argument("output_filepath", type=click.Path())
+# def main(input_filepath, output_filepath):
+def main(dimensions):
+    # logger = logging.getLogger(__name__)
+    # logger.info("making final data set from raw data")
+    path_to_glove_pkl = ".\\data\\interim\\filtered_embeddings.pkl" # TODO : Add to .env
+    embeddings_index = parse_glove_data(
+        f".\\data\\raw\\glove.6B.{dimensions}d.txt", progress_bar=True
+    )
+    words_array = get_words_array(embeddings_index)
+    words_array = filter_alphabetic(words_array)
+    words_array = filter_20k(words_array, ".\\data\\external\\20k.txt")
 
-
-@click.command()
-@click.argument("input_filepath", type=click.Path(exists=True))
-@click.argument("output_filepath", type=click.Path())
-def main(input_filepath, output_filepath):
-    """Runs data processing scripts to turn raw data from (../raw) into
-    cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info("making final data set from raw data")
+    glove_df = embeddings_to_dataframe(embeddings_index, words_array)
+    glove_df.to_pickle(path_to_glove_pkl)
 
 
 if __name__ == "__main__":
     log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
+    # TODO : Explore if this can be used to remove os/sys from run_simulations.py
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
 
--- src/simulations/run_simulations.py
+++ src/simulations/run_simulations.py
@@ -7,67 +7,35 @@ from tqdm import tqdm
 DATASET_OPTIONS = ["50", "100", "200", "300"]
 
 
-def _cosine_similarity(a, b):
-    """
-    Calculate the cosine similarity between two vectors.
-
-    Parameters:
-    - a (numpy.array): First vector.
-    - b (numpy.array): Second vector.
-
-    Returns:
-    - float: Cosine similarity score between the two vectors.
-    """
-    dot_product = np.dot(a, b)
-    norm_a = np.linalg.norm(a)
-    norm_b = np.linalg.norm(b)
-    if norm_a == 0 or norm_b == 0:
-        return 0  # Handling division by zero
-    return dot_product / (norm_a * norm_b)
+def _euclidean_distance(a, b):
+    return np.sqrt(np.sum(np.square(a - b)))
 
 
-def _get_similarities_series(glove_df, vector):
-    return glove_df.apply(lambda row: _cosine_similarity(row, vector), axis=1)
+def _get_distances_series(glove_df, vector):
+    return np.sqrt(np.sum(np.square(glove_df - vector), axis=1))
 
 
 def _random_similar_word(word, glove_df, similarity_threshold=200):
-    """
-    Get a random word similar to the given word from a GloVe embeddings DataFrame.
-
-    Parameters:
-    - word (str): The word to find similar words to.
-    - glove_df (pandas.DataFrame): DataFrame containing GloVe embeddings with words as index.
-    - similarity_threshold (int): The maximum number of similar words to consider.
-
-    Returns:
-    - str: A random word similar to the given word.
-    """
-    similarities = _get_similarities_series(glove_df, glove_df.loc[word])
-    sorted_similarities = similarities.argsort()[::-1]
-    filtered_glove = glove_df.index[sorted_similarities[1 : similarity_threshold + 1]]
-    similar_word = np.random.choice(filtered_glove)
+    distances = _get_distances_series(glove_df, glove_df.loc[word])
+    sorted_distances = distances.sort_values()
+    smallest_distances = sorted_distances.iloc[1 : similarity_threshold + 1]
+    similar_word = np.random.choice(smallest_distances.index)
     return similar_word
+
+def _get_result_vector():
+    # TODO Create function
+    ...
 
 
 def _pick_final_word(
     result_vector, starting_word, intermediate_words, glove_df, num_operations
 ):
-    """
-    Pick a final word for an equation generated using word vectors.
-
-    Parameters:
-    - result_vector (numpy.array): Resultant vector of the equation.
-    - starting_word (str): The starting word of the equation.
-    - intermediate_words (list of str): List of intermediate words used in the equation.
-    - glove_df (pandas.DataFrame): DataFrame containing GloVe embeddings with words as index.
-    - num_operations (int): Number of operations in the equation.
-
-    Returns:
-    - str: The final word selected for the equation.
-    """
+    # TODO : Simplify function, lessen arguments, pick from filtered group of words
     equation_words = [starting_word] + list(intermediate_words)
-    sorted_glove_indices = _get_similarities_series(glove_df, result_vector).argsort()[::-1]
-    closest_words = glove_df.index[sorted_glove_indices[: num_operations + 2]]
+    sorted_glove_indices = (
+        _get_distances_series(glove_df, result_vector).sort_values().index
+    )
+    closest_words = sorted_glove_indices[: num_operations + 2]
 
     for final_word in closest_words:
         if final_word not in equation_words:
@@ -76,18 +44,6 @@ def _pick_final_word(
 
 
 def _equation_to_string(starting_word, intermediate_words, final_word, operations):
-    """
-    Convert an equation represented by its components into a string format.
-
-    Parameters:
-    - starting_word (str): The starting word of the equation.
-    - intermediate_words (list of str): List of intermediate words used in the equation.
-    - final_word (str): The final word of the equation.
-    - operations (list of int): List of operations (+1 for addition, -1 for subtraction).
-
-    Returns:
-    - str: The equation string in the format "starting_word + intermediate_word1 - intermediate_word2 = final_word".
-    """
     equation_string = starting_word
 
     for i, operation in enumerate(operations):
@@ -98,18 +54,9 @@ def _equation_to_string(starting_word, intermediate_words, final_word, operation
 
 
 def simulate_game(glove_df, num_operations):
-    """
-    Simulate a word-based game using GloVe embeddings.
-
-    Parameters:
-    - glove_df (pandas.DataFrame): DataFrame containing GloVe embeddings with words as index.
-    - num_operations (int): Number of operations to perform in the game.
-
-    Returns:
-    - tuple: A tuple containing the similarity score between the final word and the result vector,
-             and the equation string representing the game.
-    """
+    # TODO : Simplify function with more subfunctions
     starting_word = np.random.choice(glove_df.index)
+
     intermediate_words = np.array(
         [
             _random_similar_word(starting_word, glove_df, 100)
@@ -126,7 +73,7 @@ def simulate_game(glove_df, num_operations):
     final_word = _pick_final_word(
         result_vector, starting_word, intermediate_words, glove_df, num_operations
     )
-    similarity = _cosine_similarity(glove_df.loc[final_word], result_vector)
+    similarity = _euclidean_distance(glove_df.loc[final_word], result_vector)
 
     return similarity, _equation_to_string(
         starting_word, intermediate_words, final_word, operations
@@ -136,30 +83,18 @@ def simulate_game(glove_df, num_operations):
 def batch_simulations(
     threshold, num_results, output_filepath, glove_df, num_operations
 ):
-    """
-    Perform batch simulations of word-based games using GloVe embeddings.
-
-    Parameters:
-    - threshold (float): Minimum similarity score for considering a game result.
-    - num_results (int): Number of simulation results to generate.
-    - output_filepath (str): Filepath to save the results.
-    - glove_df (pandas.DataFrame): DataFrame containing GloVe embeddings with words as index.
-    - num_operations (int): Number of operations to perform in each game.
-
-    Returns:
-    - None
-    """
+    # TODO : Reduce arguments if possible
     simulation_results = []
 
     with tqdm(total=num_results, desc="Equations found") as pbar:
         while len(simulation_results) < num_results:
             sim = simulate_game(glove_df, num_operations)
-            if sim[0] > threshold:
+            if sim[0] < threshold:
                 simulation_results.append(sim)
                 pbar.update(1)
-    results_df = pd.DataFrame(simulation_results, columns=["similarity", "equation"])
+    results_df = pd.DataFrame(simulation_results, columns=["distance", "equation"])
 
-    sorted_results = results_df.sort_values(by="similarity", ascending=False)
+    sorted_results = results_df.sort_values(by="distance", ascending=True)
     sorted_results.to_csv(output_filepath)
 
 
@@ -167,18 +102,11 @@ def batch_simulations(
 @click.argument("dimensions", type=click.Choice(DATASET_OPTIONS))
 @click.argument("output_filepath", type=click.Path())
 def main(dimensions, output_filepath):
-    embeddings_index = make_dataset.parse_glove_data(
-        f".\\data\\raw\\glove.6B.{dimensions}d.txt", True
-    )
-
-    words_array = make_dataset.get_words_array(embeddings_index)
-    print("len(words_array) (raw):\t\t\t" + "{:,}".format(len(words_array)))
-    words_array = make_dataset.filter_alphabetic(words_array)
-    print("len(words_array) (filter_alphabetic):\t" + "{:,}".format(len(words_array)))
-    words_array = make_dataset.filter_20k(words_array, ".\\data\\external\\20k.txt")
-    print("len(words_array) (filter_20k):\t\t" + "{:,}".format(len(words_array)))
-
-    glove_df = make_dataset.embeddings_to_dataframe(embeddings_index, words_array)
+    path_to_glove_pkl = ".\\data\\interim\\filtered_embeddings.pkl" # TODO : Add to .env
+    if not os.path.exists(path_to_glove_pkl):
+        make_dataset.main(dimensions)
+    glove_df = pd.read_pickle(path_to_glove_pkl)
+        
 
     batch_simulations(30, 100, output_filepath, glove_df, 2)
 
